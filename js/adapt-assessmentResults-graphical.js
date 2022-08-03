dcfdc5d2584b06a773b1dc10bbc771ecb79f8f29
Volkmar Breitfeld <vbreitfeld@flickrocket.com>
2022-08-03 11:46:54 +0200

    Fixed upper to lower case issue with file name

--- js/adapt-assessmentResults-graphical.js
+++ js/adapt-assessmentResults-graphical.js
@@ -1,6 +1,6 @@
 import components from 'core/js/components';
-import AssessmentResultsGraphicalView from './AssessmentResultsGraphicalView';
-import AssessmentResultsGraphicalModel from './AssessmentResultsGraphicalModel';
+import AssessmentResultsGraphicalView from './assessmentResultsGraphicalView';
+import AssessmentResultsGraphicalModel from './assessmentResultsGraphicalModel';
 
 export default components.register('assessmentResults-graphical', {
   // create a new class in the inheritance chain so it can be extended per component type if necessary later
